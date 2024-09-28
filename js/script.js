cf39803d66bdd04dcefa92d8f585039181e356e5
Hugo JAUBERT <hjaubert7@gmail.com>
2024-09-28 17:05:35 +0200

    Button Settings added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-let beginMinutes = 1
-let workTime = beginMinutes * 60
+let workTime = 10
+let breakTime = 5
+let time = workTime * 60
 let isBreak = true
 let isRunning = false
 let loop
@@ -8,9 +9,12 @@ const timerElement = document.getElementById("timer")
 const buttonLaunch = document.getElementById("button")
 
 function display(){
-    let minutes = parseInt(workTime / 60, 10)
-    let seconds = parseInt(workTime % 60, 10)
+    let minutes = parseInt(time / 60, 10)
+    let seconds = parseInt(time % 60, 10)
     seconds  = "0" + seconds
+    if(workTime < 10){
+        minutes = "0" + minutes
+    }
     timerElement.innerText = `${minutes}:${seconds}`
 }
 
@@ -18,34 +22,34 @@ function launchTravail(){
     if(!isRunning){
         loop = setInterval(() => {
             isRunning = true
-            let minutes = parseInt(workTime / 60, 10)
-            let seconds = parseInt(workTime % 60, 10)                
+            let minutes = parseInt(time / 60, 10)
+            let seconds = parseInt(time % 60, 10)                
             minutes = minutes < 10 ? "0" + minutes : minutes
             seconds = seconds < 10 ? "0" + seconds : seconds    
             timerElement.innerText = `${minutes}:${seconds}`
         
-            if(workTime == 0){
+            if(time == 0){
         
                 if(isBreak == true){
                     document.getElementById("background").style.background = "green"
+                    document.getElementById("workBreak").style.background = "green"
                     document.getElementById("break").style.color = "white"
                     document.getElementById("work").style.color = "grey"
-                    beginMinutes = 2
-                    workTime = beginMinutes * 60
+                    time = breakTime * 60
                     isBreak = false
                 } else {
                     document.getElementById("background").style.background = "rgb(207, 1, 1)"
+                    document.getElementById("workBreak").style.background = "rgb(207, 1, 1)"
                     document.getElementById("work").style.color = "white"
                     document.getElementById("break").style.color = "grey"
-                    beginMinutes = 1
-                    workTime = beginMinutes * 60
+                    time = workTime * 60
                     isBreak = true
                 }
             }
         
-            workTime = workTime <= 0 ? 0 : workTime - 1
+            time = time <= 0 ? 0 : time - 1
         
-        }, 100)
+        }, 10)
     }
 }
 
@@ -57,8 +61,7 @@ buttonLaunch.addEventListener('click', () => {
     } else {
         clearInterval(loop)
         isRunning = false
-        workTime = beginMinutes * 60
-        display()
+        location.reload()
         buttonLaunch.classList.remove('fa-arrow-rotate-left')
         buttonLaunch.classList.add('fa-play')
     }
