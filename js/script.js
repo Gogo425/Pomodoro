a3cb7898bd83dfebc02dd5a8484abb9c98e0ef98
Hugo JAUBERT <hjaubert7@gmail.com>
2024-10-01 22:40:36 +0200

    LocalStorage added, javasdoc and css doc added and few improvements

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Variable declaration for the work time, break time, booleans for certains test and the variable loop for the setInterval function
 let workTime = localStorage.getItem("inputWork") || 25
 let breakTime = localStorage.getItem("inputBreak") || 5
 let isBreak = true
@@ -5,65 +6,26 @@ let isRunning = false
 let isForm = false
 let loop
 
+//Declaration of the constants which represents all the HTML elements that needs to be interacted with
 const timerElement = document.getElementById("timer")
 const buttonLaunch = document.getElementById("buttonStart")
-const formulaire = document.getElementById("form")
+const formulaire = document.getElementById("formulary")
 const settingsButton = document.getElementById("settings")
-const exitButton = document.getElementById("buttonExit")
 const numberWork = document.getElementById("numberWork")
 const numberBreak = document.getElementById("numberBreak")
 const inputBreak = document.getElementById("newTimeBreak")
 const inputWork = document.getElementById("newTimeWork")
-const submitButton = document.getElementById("submitButton")
+const submitButton = document.getElementById("check")
 
-function display(){
-    let time = workTime * 60
-    let minutes = parseInt(time / 60, 10)
-    let seconds = parseInt(time % 60, 10)
-    seconds  = "0" + seconds
-    if(workTime < 10){
-        minutes = "0" + minutes
-    }
-    timerElement.innerText = `${minutes}:${seconds}`
-}
+//the 3 main functions
+launch()
+display()
+form()
 
-function timer(){
-    let time = workTime * 60
-    form()
-    if(isRunning == false && isForm == false){
-        loop = setInterval(() => {
-            isRunning = true
-            let minutes = parseInt(time / 60, 10)
-            let seconds = parseInt(time % 60, 10)                
-            minutes = minutes < 10 ? "0" + minutes : minutes
-            seconds = seconds < 10 ? "0" + seconds : seconds    
-            timerElement.innerText = `${minutes}:${seconds}`
-        
-            if(time == 0){
-        
-                if(isBreak == true){
-                    document.getElementById("background").style.background = "green"
-                    document.getElementById("workBreak").style.background = "green"
-                    document.getElementById("break").style.color = "white"
-                    document.getElementById("work").style.color = "grey"
-                    time = breakTime * 60
-                    isBreak = false
-                } else {
-                    document.getElementById("background").style.background = "rgb(207, 1, 1)"
-                    document.getElementById("workBreak").style.background = "rgb(207, 1, 1)"
-                    document.getElementById("work").style.color = "white"
-                    document.getElementById("break").style.color = "grey"
-                    time = workTime * 60
-                    isBreak = true
-                }
-            }
-        
-            time = time <= 0 ? 0 : time - 1
-        
-        }, 10)
-    }
-}
-
+/**
+ * @author Hugo JAUBERT
+ * The function which when the ButtonLaunch is pressed calls the function timer() and switches the button icon
+ */
 function launch(){
     buttonLaunch.addEventListener('click', () => {
         if(isForm == false){
@@ -82,25 +44,82 @@ function launch(){
     })
 }
 
-function edit_time(){
-
-    inputBreak.addEventListener('change', ()=> {
-        if(inputBreak.value < 120 && inputBreak.value > 1){
-            localStorage.setItem("inputBreak", inputBreak.value)
-            breakTime = inputBreak.value
-            display()
-        }
-    })
-
-    inputWork.addEventListener('change', ()=> {
-        if(inputWork.value < 120 && inputWork.value > 1){
-            localStorage.setItem("inputWork", inputWork.value)
-            workTime = inputWork.value
-            display()
-        }
-   })
+/**
+ * @author Hugo JAUBERT
+ * Allows to show the timer with the minutes and the seconds
+ */
+function display(){
+    let time = workTime * 60
+    let minutes = parseInt(time / 60, 10)
+    let seconds = parseInt(time % 60, 10)
+    seconds  = "0" + seconds
+    if(workTime < 10){
+        minutes = "0" + minutes
+    }
+    timerElement.innerText = `${minutes}:${seconds}`
 }
 
+/**
+ * @author Hugo JAUBERT
+ * Starts an interval to make a timer with the given numbers which decreases every second
+ */
+function timer(){
+    let time = workTime * 60
+    form()
+    if(isRunning == false && isForm == false){
+        loop = setInterval(() => {
+            isRunning = true
+            let minutes = parseInt(time / 60, 10)
+            let seconds = parseInt(time % 60, 10)                
+            minutes = minutes < 10 ? "0" + minutes : minutes
+            seconds = seconds < 10 ? "0" + seconds : seconds    
+            timerElement.innerText = `${minutes}:${seconds}`
+        
+            if(time == 0){
+        
+                if(isBreak == true){
+                    toBreakColor()
+                    time = breakTime * 60
+                    isBreak = false
+                } else {
+                    toWorkColor()
+                    time = workTime * 60
+                    isBreak = true
+                }
+            }
+        
+            time = time <= 0 ? 0 : time - 1
+        
+        }, 10)
+    }
+}
+
+/**
+ * @author Hugo JAUBERT
+ * Switches the color to green when the work time has reached 0
+ */
+function toBreakColor(){
+    document.getElementById("background").style.background = "green"
+    document.getElementById("workBreak").style.background = "green"
+    document.getElementById("break").style.color = "white"
+    document.getElementById("work").style.color = "grey"
+}
+
+/**
+ * @author Hugo JAUBERT
+ * Switches the color to red when the break time has reached 0
+ */
+function toWorkColor(){
+    document.getElementById("background").style.background = "rgb(207, 1, 1)"
+    document.getElementById("workBreak").style.background = "rgb(207, 1, 1)"
+    document.getElementById("work").style.color = "white"
+    document.getElementById("break").style.color = "grey"
+}
+
+/**
+ * @author Hugo JAUBERT
+ * The main function which handles the formulary
+ */
 function form(){
         buttonLaunch.addEventListener('click', ()=> {
             if(isForm == true){
@@ -108,28 +127,51 @@ function form(){
             }
         })
 
-        settingsButton.addEventListener('click', ()=> {
-            if(!isRunning){
-                isForm = true;
-                formulaire.style.display = "block"
-
-                
-            }
-        })
+        display_form()
 
         edit_time();
-
-        exitButton.addEventListener('click', ()=> {
-            isForm = false;
-            if(!isRunning){
-                formulaire.style.display = "none"
-            }
-        })
 }
 
+/**
+ * @author Hugo JAUBERT
+ * Handles whenthe form should appear
+ */
+function display_form(){
+    settingsButton.addEventListener('click', ()=> {
+        if(!isRunning){
+            isForm = true;
+            formulaire.style.display = "block"             
+        }
+    })
 
-form()
-launch()
-display()
+    submitButton.addEventListener('click', ()=> {
+        isForm = false;
+        if(!isRunning){
+            formulaire.style.display = "none"
+        }
+    })
+}
+
+/**
+ * @author Hugo JAUBERT
+ * When the user enters a value in the input zone, it is set with the LocalStorage 
+ */
+function edit_time(){
+    inputBreak.addEventListener('change', ()=> {
+        if(inputBreak.value <= 120 && inputBreak.value >= 1){
+            localStorage.setItem("inputBreak", inputBreak.value)
+            breakTime = inputBreak.value
+            display()
+        }
+    })
+
+    inputWork.addEventListener('change', ()=> {
+        if(inputWork.value <= 120 && inputWork.value >= 1){
+            localStorage.setItem("inputWork", inputWork.value)
+            workTime = inputWork.value
+            display()
+        }
+    })
+}
 
 
